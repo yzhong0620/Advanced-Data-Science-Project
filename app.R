1c40758430506f8a001e04ec2e31525ca35be4ed
yzhong0620 <yzhong1@macalester.edu>
2021-11-18 10:20:20 -0600

    shiny added

---
+++ app.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(shiny)
+library(bslib)
+
+data <- read_csv("data.csv")
+
+ui <- fluidPage(
+  theme = bs_theme(primary = "#ADD8E6", 
+                   secondary = "#FFEBCD", 
+                   base_font = list(font_google("Raleway"), "-apple-system", 
+                                    "BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", 
+                                    "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", 
+                                    "Segoe UI Symbol"), 
+                   bootswatch = "spacelab"),
+
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "disease", # to use in code
+                  label = "Disease:", # how it looks in UI
+                  choices = c("asthma_er_avg", "cancer_incidence_rate")
+      ),
+      selectInput(inputId = "pollutant", # to use in code
+                  label = "Pollutant:", # how it looks in UI
+                  choices = c("annual_mean_co", "annual_mean_no2", "annual_mean_ozone", "annual_mean_pb", "annual_mean_pm25", "annual_mean_pm10", "annual_mean_so2")
+      )
+      
+    ),
+
+    mainPanel(
+      plotOutput(outputId = "line_graph")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$line_graph <- renderPlot({
+    disease <- input$disease
+    pollutant <- input$pollutant
+    data %>% 
+      filter(!(COUNTY == "California")) %>% 
+      ggplot(aes(x = .data[[pollutant]], y = .data[[disease]])) +
+      geom_point() +
+      geom_smooth(method = "lm") +
+      labs(title = "disease vs pollutant", x = "", y = "")
+  })
+}
+
+shinyApp(ui = ui, server = server)
